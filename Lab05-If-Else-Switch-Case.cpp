f371825efa70c3dc241980bfd917c42bd47eb724
67543206073-8 <62295657+IQMET@users.noreply.github.com>
2024-08-09 00:09:29 +0700

    Add files via upload

---
+++ Lab05-If-Else-Switch-Case.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    char score;
+    int x
+    printf("hello\n")
+    printf("please enter number only\n")
+    switch (score/5)
+    case x
+
+
+
+
+
+
+    if()
+
+       
+    
+    
+    
+
+   
+       
