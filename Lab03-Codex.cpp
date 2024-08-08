0d65e89462df19d68ada39e222913b66decf684f
67543206073-8 <62295657+IQMET@users.noreply.github.com>
2024-08-08 23:54:34 +0700

    Update Lab03-Codex.cpp

--- Lab03-Codex.cpp
+++ Lab03-Codex.cpp
@@ -3,8 +3,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-void main(int)
-{
+void main(int){
     char CustomerName[15], Name[10][15];
     int Quantity[10], NumberOfItems, i;
     float UnitPrice[10], TotalPrice[10], TotalAmount;
