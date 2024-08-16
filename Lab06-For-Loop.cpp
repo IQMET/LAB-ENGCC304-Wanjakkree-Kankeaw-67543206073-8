19ead0479b05deaacc8d5357287ce571480a1310
67543206073-8 <62295657+IQMET@users.noreply.github.com>
2024-08-16 12:58:57 +0700

    Add files via upload

---
+++ Lab06-For-Loop.cpp
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main() {
+    int N;
+    int i;
+
+    printf("Enter value:");
+    scanf("%d", &N);
+
+    if (N%2== 0) {
+        printf("Series:");
+        for (i = N; i >= 0; i -= 2) {
+            printf("%d ", i);
+        }
+    } else {
+        printf("Series:");
+        for (i = 1; i <= N; i += 2) {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}//end function
