bb1b035850b8557620e1b6cbcdb19c0c98494e1e
67543206073-8 <62295657+IQMET@users.noreply.github.com>
2024-09-07 21:03:04 +0700

    Update lab7.cpp

--- lab7.cpp
+++ lab7.cpp
@@ -11,40 +11,43 @@ int main(){
     int high = 100 ;
     int x = 100 ;
 
-    srand(time(NULL)) ;
-    printf(" Do you want to play game (1=play,-1=exit) ") ;
-    scanf("%d", &n) ;
+    srand( time(NULL) ) ;
+    printf( "Do you want to play game (1=play,-1=exit)" ) ;
+    scanf( "%d" , &n) ;
 
     if (n == 1)
     {
         random = rand() % 100 + 1 ;
-        printf(" Score=%d\n ", x) ;
+        printf( "Score=%d\n" , x) ;
         do
         {
-            printf (" Guess the winning number (%d-%d)\n ", low , high ) ;
-            scanf ("%d" , &Number) ;
+            printf ( "Guess the winning number (%d-%d)\n" , low , high ) ;
+            scanf ( "%d" , &Number) ;
 
-            if (Number != random){
+            if ( Number != random ){
                 x = x - 10 ;
-                if (Number > random)
+                if ( Number > random )
                 {
-                    printf (" Sorry, the winning number is LOWER than %d. (Score=%d)\n ", Number, x ) ;
-                    high = Number - 1 ;
+                    printf ( "Sorry, the winning number is LOWER than %d. (Score=%d)\n" , Number, x ) ;
+                    if ( Number < high ){ 
+                        high = Number - 1 ;
+                    }
                 }//end if
-                else if (Number < random)
+                else if ( Number < random )
                 {
-                    printf (" Sorry, the winning number is HIGHER than %d. (Score=%d)\n ", Number, x ) ;
-                    low = Number + 1 ;
+                    printf ( "Sorry, the winning number is HIGHER than %d. (Score=%d)\n" , Number, x ) ;
+                    if ( Number > low ){
+                        low = Number + 1 ;
+                    }
                 }//end else if
-                if (x <= 0)
+                if ( x <= 0)
                 {
-                    printf (" nah you lose\n ") ;
+                    printf ( "nah you lose\n" ) ;
                     break ;
                 }//end if
             }//end if
-        } while (Number != random);
-        //end do while
-        printf ("The winning number is %d. Score this game: %d\n", random, x) ;
+        } while ( Number != random ) ; //end do while
+        printf ( "The winning number is %d. Score this game: %d\n" , random, x) ;
     }// end if
 
     return 0 ;
